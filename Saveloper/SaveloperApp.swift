a179a163743fd4593f103ab47564e36535c96d87
Denis Svetlakov <dps150482@gmail.com>
2022-11-08 13:36:59 +1000

    Initial Commit

---
+++ Saveloper/SaveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveloperApp.swift
+//  Saveloper
+//
+//  Created by Denis Svetlakov on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SaveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
